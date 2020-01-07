b995ba7df3f5743e518afe2be15e3b7da3016dfe
paul wisehart <paul@oldcode.org>
2020-01-07 08:32:52 -0500

    asd

--- clicktrack.c
+++ clicktrack.c
@@ -18,17 +18,31 @@ rand_0_1()
 	return (rand() % 2);
 }
 
+char	lr = 'l';
 void
 send_msg_click(PortMidiStream * pm_stream)
 {
 	char 		chan = 9;	/* chan 9 is drums */
 	int 		d2 = 120;	/* intensity */
 	int 		midi_note = 76;	/* 76 77  low, high wood block */
+	int		usl = 100000;
+
+	int		hh = 42;
 
 	midi_note = 76 + rand_0_1();
-	Pm_WriteShort(pm_stream, 0, Pm_Message(MIDI_NOTE_ON | chan, midi_note, d2));
-	usleep(100000);
-	Pm_WriteShort(pm_stream, 0, Pm_Message(MIDI_NOTE_OFF | chan, midi_note, d2));
+	//usl = usl * (rand() % 3);
+
+	if (lr == 'l') {
+		Pm_WriteShort(pm_stream, 0, Pm_Message(MIDI_NOTE_ON | chan, midi_note, d2));
+		usleep(usl);
+		Pm_WriteShort(pm_stream, 0, Pm_Message(MIDI_NOTE_OFF | chan, midi_note, d2));
+		lr = 'r';
+	} else {
+		lr = 'l';
+		Pm_WriteShort(pm_stream, 0, Pm_Message(MIDI_NOTE_ON | chan, hh, d2));
+		usleep(usl);
+		Pm_WriteShort(pm_stream, 0, Pm_Message(MIDI_NOTE_OFF | chan, hh, d2));
+	}
 }
 
 void
@@ -44,7 +58,7 @@ loop_wood_block()
 
 	for (i = 0; i < 9000; i++) {
 		usleep(100000);
-		printf("click - %d %d\n", i, rand_0_1());
+		printf("click - %d %d %d %d\n", i, i%4, i%8, i%16);
 		send_msg_click(pm_stream);
 	}
 
