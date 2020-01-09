d046d2c9e293746824f97e265702a46302ebc3ed
paul wisehart <paul@oldcode.org>
2020-01-09 02:05:39 -0500

    wizard fight has a lot of good basic ncurses stuff

--- wizardfight.c
+++ wizardfight.c
@@ -5,17 +5,70 @@
 
 #include "mycur.h"
 
-
 int
 main()
 {
+	WINDOW         *arena;
+	int 		c;
+
 	mycur_init();
 
-	move(2, 2);
-	printw("Wizard Fight!");
-	refresh();
+	arena = newwin(30, 80, 4, 14);
+	if (arena == NULL) {
+		move(2, 2);
+		printw("ERROR with arena newwin");
+	}
+	while ((c = getch())) {
+		switch (c) {
+		case 'q':
+			goto end;
+		case 'w':
+			wborder(arena, 0, 0, 0, 0, 0, 0, 0, 0);
+			curs_set(0);
+			wrefresh(arena);
+			break;
+		case 't':
+			wmove(arena, 2, 2);
+			waddch(arena, 'X');
+			waddch(arena, 'X');
+			waddch(arena, 'X');
+			waddch(arena, 'X');
+			waddch(arena, 'X');
+			waddch(arena, 'X');
+			curs_set(0);
+			wrefresh(arena);
+			break;
+		case 'p':
+			move(2, 2);
+			printw("Wizard Fight!");
+			refresh();
 
-	usleep(1000000);
+			wmove(arena, 10, 10);
+			waddch(arena, 'W');
+			wmove(arena, 11, 10);
+			waddch(arena, '1');
+			wrefresh(arena);
+
+			wmove(arena, 20, 40);
+			waddch(arena, 'W');
+			wmove(arena, 21, 40);
+			waddch(arena, '2');
+			curs_set(0);
+			wrefresh(arena);
+			break;
+		case 'c':
+			wclear(arena);
+			wrefresh(arena);
+			break;
+		case 'C':
+			clear();
+			refresh();
+			break;
+		}
+	}
+
+end:
+	delwin(arena);
 	endwin();
 	return EXIT_SUCCESS;
 }
